be3e509d576ac72866db74549255ee182d79bf6c
JuliaGCandotti <jucandotti@gmail.com>
2025-01-20 16:12:04 -0700

    Final edits
    Construction of the assignment requirements.

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div>
-      <h1>A</h1>
-      <p>B</p>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <li>
+        <Link href="/week-2">Go to Week-2 page</Link>
+      </li>
     </div>
   )
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <p>My name is Julia Candotti. This link <Link href="https://github.com/JuliaGCandotti/cprg306-assignments">(click here)</Link> will take you to my Github repository.
+        </p>
+    )
+}
